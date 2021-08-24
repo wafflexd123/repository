1abab45dcdbb4af81f62d3167024a22caff7b874
wafflexd123 <54018924+wafflexd123@users.noreply.github.com>
2021-08-25 09:27:47 +1000

    100%

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,14 +6,17 @@ public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
     int i = 3;
+    int random;
     void Start()
     {
-
+        random = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log($"{name}:{++i}");
+        if (CompareTag("Red") && i == 100) gameObject.SetActive(false);
+        else if (CompareTag("Blue") && i == random) rend.enabled = false;
     }
 }
