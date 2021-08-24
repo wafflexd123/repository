dd99d25f5fafe080ae0cad758bbba805d67ba672
wafflexd123 <54018924+wafflexd123@users.noreply.github.com>
2021-08-25 09:15:18 +1000

    90%

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Renderer rend;
     int i = 3;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
